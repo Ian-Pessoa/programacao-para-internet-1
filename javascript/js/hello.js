ba65e697fec80bc37fcb5419c12b5330fee8526e
Ian Pessôa <105527360+Ian-Pessoa@users.noreply.github.com>
2023-10-04 20:32:59 -0300

    Inicio dos estudo de javascript

---
+++ javascript/js/hello.js
@@ -0,0 +1,37 @@
+//alert("Hello World!")
+console.log("Hello World!")
+
+// Variáveis
+
+if (true) {
+  let x ="olá"
+  console.log(x)
+}
+let x ="mundo"
+console.log(x)
+const y = 32
+
+// Tipagem
+
+let idade = 32
+let peso = 69.5
+let nome = "João"
+let sobrenome = 'Doe'
+
+let temSol = true
+let temChuva = false
+let z //undefined
+let pessoa = null
+
+let frutasPreferidas = ["Maçã", "Laranja", "Melancia"]
+let teste = ["Carro", 1, 1.25]
+console.log(frutasPreferidas)
+console.log(teste)
+
+//NaN
+
+let objetoNumero = parseInt(12)
+console.log(objetoNumero)
+
+let objetoPalavra = parseInt("Eu")
+console.log(objetoPalavra)
