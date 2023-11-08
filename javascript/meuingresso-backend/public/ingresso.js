3f558a2f7b148b37363813b43e9fc3246c46d6e6
IAN <ian.pessoaa@gmail.com>
2023-11-08 20:28:39 -0300

    backend

--- javascript/meuingresso-backend/public/ingresso.js
+++ javascript/meuingresso-backend/public/ingresso.js
@@ -8,56 +8,33 @@ let carrinhoDeCompras = JSON.parse(sessionStorage.getItem("carrinho")) || [];
 
 window.addEventListener("load", main)
 
-const filmes = [
-    {
-        id: 0,
-        img: "https://4.bp.blogspot.com/-GW1Ex5rl1o8/VMG9B8zuM2I/AAAAAAAAQ-A/PdXFAKUlN2c/s1600/somaoredor1.jpeg",
-        titulo: "O Som ao redor",
-        descricao:
-            "O Som ao Redor começa com a chegada de uma milícia a uma rua de classe média da cidade do Recife, onde    diferentes narrativas acabam se cruzando. Segundo a Associação Brasileira de Críticos de Cinema (Abraccine), é o 15º melhor filme da história do cinema nacional.",
-        ano: 2013,
-        direcao: "Kleber Mendonça",
-        genero: "Suspense, Drama",
-        valor: "3,50",
-    },
-    {
-        id: 1,
-        img: "https://upload.wikimedia.org/wikipedia/pt/2/29/Central_do_Brasil_poster.jpg",
-        titulo: "Central do Brasil",
-        descricao:
-            "Dora, uma ex-professora que escreve cartas na Central do Brasil, e o menino Josué, que fica órfão da noite para o dia. O filme, que emocionou o mundo, recebeu duas indicações ao Oscar: nas categorias melhor filme estrangeiro e melhor atriz.",
-        ano: 1998,
-        direcao: "Walter Salles",
-        genero: "Drama",
-        valor: "4,50",
-    },
-    {
-        id: 2,
-        img: "https://upload.wikimedia.org/wikipedia/pt/thumb/1/10/CidadedeDeus.jpg/220px-CidadedeDeus.jpg",
-        titulo: "Cidade de Deus",
-        descricao:
-            'Nos anos 1960, a favela é um complexo habitacional recém-construído longe do centro do Rio de Janeiro, com pouco acesso à eletricidade e água. Três ladrões amadores conhecido como "Trio Ternura" — Cabeleira, Alicate e Marreco — aterrorizam os negócios locais. Marreco é o irmão de Buscapé. Como Robin Hood, eles dividem parte do dinheiro roubado com os habitantes da favela chamada de Cidade de Deus e, em troca, são protegidos por eles.',
-        ano: 2002,
-        direcao: "Fernando Meirelles",
-        genero: "Drama, Ação",
-        valor: "3,50",
-    },
-    {
-        id: 3,
-        img: "https://upload.wikimedia.org/wikipedia/pt/thumb/2/2a/TropaDeElitePoster.jpg/250px-TropaDeElitePoster.jpg",
-        titulo: "Tropa de Elite",
-        descricao:
-            "Os acontecimentos do filme são narrados em primeira pessoa pelo Capitão Roberto Nascimento, dando uma perspectiva ao espectador de todos os fatos interligados. O filme começa in medias res, no ano de 1997, em um baile funk no Morro da Babilônia, uma das principais bases do narcotráfico da cidade Rio de Janeiro.",
-        ano: 2007,
-        direcao: "José Padilha",
-        genero: "Drama, Policial",
-        valor: "4,99",
-    },
-];
+let filmes = [];
 
+function imprimeMensagemPronta(resultadoDaPromessa) {
+    console.log('Dados obtidos!');
+    return resultadoDaPromessa.json();
+}
 
+function imprimeMensagemConvertida(resultado) {
+    console.log(resultado);
+    filmes = resultado;
+    renderizaDadosNaTela();
+}
 
-function main() {
+// Syntax Sugar
+
+// ASYNC / AWAIT => converte para promise
+async function main() {
+    // CONSUMIR A API DE FILMES
+    const resultado = await fetch("http://localhost:3333/api/filmes");
+    const converterResultadoParaJson = await resultado.json();
+    console.log(converterResultadoParaJson);
+
+    filmes = converterResultadoParaJson;
+    renderizaDadosNaTela();
+}
+
+function renderizaDadosNaTela() {
     // Adicionar os filmes na div 
     const divFilmes = document.getElementById("filmes");
 
@@ -108,12 +85,9 @@ function main() {
         // img, p, ul, 4 li, button
     }
 
-
     // Renderize os elementos de SessionStorage no elemento HTML carrinho
 
-    renderizarCarrinho()
-
-
+    renderizarCarrinho()    
 }
 
 
@@ -149,6 +123,23 @@ function renderizarCarrinho() {
         total.innerText = `Total: R$ ${valorFinal.toFixed(2).replace(".", ",")}`
     }
 
+    // obter o botao finalizar compra e adicionar o evento click
+
+    const botaoFinalizarCompra = document.getElementById('finalizar-compra');
+    botaoFinalizarCompra.addEventListener("click", enviarDadosParaOBackend);1
+}
+
+async function enviarDadosParaOBackend(evento) {
+    // realizar fetch com método PUSH, encaminhnado o array de string
+
+    const resultado = fetch("http://localhost:3333/api/carrinho"), {
+        method: 'POST',
+        body: {
+            dados: JSON.stringify:carrinhoDeCompras
+        }
+
+    }
+        console.log(resultado);
 }
 
 
@@ -248,9 +239,11 @@ function verificarBotaoFinalizarCompra(){
         if (botaoFinalizar.classList.length === 0) {
             console.log("Modificando classe do botão finalizar compra")
             botaoFinalizar.classList.add("finalizar-compra")
+            botaoFinalizar.disabled = false
         }
     } else {
         // Removo a classe finalizar compra
         botaoFinalizar.classList.remove("finalizar-compra")
+        botaoFinalizar.disabled = true
     }
 }
